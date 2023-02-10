4029c2907329f242ac9aebea937f96f3c0ed3fbe
Majed <boumaged@gmail.com>
2023-02-10 11:16:55 +0400

    Initial Commit

---
+++ CowML/CowMLApp.swift
@@ -0,0 +1,17 @@
+//
+//  CowMLApp.swift
+//  CowML
+//
+//  Created by Majed on 10/02/2023.
+//
+
+import SwiftUI
+
+@main
+struct CowMLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
